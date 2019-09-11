50af6feac36ccefb79ab875dc7e41a04b7570797
weineshet <52981354+weineshet@users.noreply.github.com>
2019-09-11 13:39:04 -0700

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb + srv://Devadmin:<passwordweine>@devcollector-pmy7b.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose =require('mongoose');
+const app = express();
+
+//Db config
+const db = require('./config/keys').mongoURI;
+//connect to mongodb
+mongoose.connect(db)
+.then(() => console.log('MongoDb connected'))
+.catch(err => console.log(err));
+//First route
+app.get ('/',(req,res) => res.send("hello!"));
+const port = 7002
+app.listen(port,()=> console.log(`Server running on port ${port}`));
