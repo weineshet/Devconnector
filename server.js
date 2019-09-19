ada51827b7d173c6d100623b93bc21f9f2c26112
weineshet <52981354+weineshet@users.noreply.github.com>
2019-09-19 09:45:41 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose =require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
+
 
 //Db config
 const db = require('./config/keys').mongoURI;
@@ -12,3 +16,7 @@ mongoose.connect(db)
 app.get ('/',(req,res) => res.send("hello!"));
 const port = 7002
 app.listen(port,()=> console.log(`Server running on port ${port}`));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts)
